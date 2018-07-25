cdc3723102024311c5bfbfbbaedf7ac3792d85c1
flippingflippers <citrussunshine2004@gmail.com>
2018-07-25 18:36:10 +0000

    automatically backed up by learn

--- ruby.rb
+++ ruby.rb
@@ -3,6 +3,6 @@ puts your_favorite_word.upcase
 puts "the banana man".upcase
 puts "IM NOT SHOUTING".downcase
 puts"supercalifragilisticesphiolidocios".length
-puts "wow + mom".length
+puts "wow"+"mom"+"supercalifragilisticesphiolidocios".length
 puts "i really like programming".capitalize
 puts "Astounding aardvarks, arguably an acient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("A","O").gsub("a","o")
